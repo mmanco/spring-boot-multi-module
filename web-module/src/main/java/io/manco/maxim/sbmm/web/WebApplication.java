21f989de498c86003f7416c48081ae94a4ee5d9b
Maxim <Maxim@192.168.1.6>
2016-02-14 16:17:09 -0500

    setting banner mode on child application

--- web-module/src/main/java/io/manco/maxim/sbmm/web/WebApplication.java
+++ web-module/src/main/java/io/manco/maxim/sbmm/web/WebApplication.java
@@ -11,9 +11,9 @@ public class WebApplication {
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder()
-			.bannerMode(Banner.Mode.OFF)
-			.sources(CoreApplication.class)
+			.parent(CoreApplication.class)
 			.child(WebApplication.class)
+			.bannerMode(Banner.Mode.CONSOLE)
 			.run(args);
 	}
 
